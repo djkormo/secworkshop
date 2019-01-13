6915a347525c5e6fa7245b410ae677c1cffa9b26
Djkormo <djkormo@gmail.com>
2019-01-13 17:55:31 +0100

    fixing

--- scripts/install-kali.sh
+++ scripts/install-kali.sh
@@ -2,5 +2,14 @@
 
 logger "Beginning installation"
 
+sudo apt-get update -y 
+sudo dpkg --configure -a # how to remove prompt
+sudo apt --fix-broken install -y
+sudo apt-get dist-upgrade -y 
+sudo apt-get install -f gdm3 -y  # how to remove prompt
+sudo apt-get install xrdp -y 
+sudo systemctl enable xrdp
+echo xfce4-session >~/.xsession
+service xrdp restart
 
 logger "Ending installation"
--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -2,7 +2,7 @@
 
 logger "Beginning installation"
 
-apt-get -y update
+sudo apt-get -y update
 sudo apt-get install lxde -y 
 sudo apt-get install xrdp -y 
 sudo /etc/init.d/xrdp start
