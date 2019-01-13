29533183bb08ca75dec53456188730b553b25a85
Djkormo <djkormo@gmail.com>
2019-01-13 15:39:39 +0100

    adding extension to ubuntu VM

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 logger "Beginning installation"
+
 apt-get -y update
+sudo apt-get install lxde -y 
+sudo apt-get install xrdp -y 
+sudo /etc/init.d/xrdp start
 
 logger "Ending installation"
